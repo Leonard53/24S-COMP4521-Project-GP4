626844a6267f634f4d401d2212d65647bf79077d
Leoanrd Yeung <94115482+Leonard53@users.noreply.github.com>
2024-04-27 19:26:59 +0800

    init: android studio initialized

---
+++ android-frontend/settings.gradle.kts
@@ -0,0 +1,24 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "COMP4521-Project-GP4"
+include(":app")
+ 
