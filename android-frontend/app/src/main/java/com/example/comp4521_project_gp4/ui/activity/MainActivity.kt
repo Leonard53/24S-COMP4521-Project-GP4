774f8633ebc48527a7ca1e784dc2b36508b8957f
Leoanrd Yeung <94115482+Leonard53@users.noreply.github.com>
2024-05-05 22:43:14 +0800

    format: width set to 2

--- android-frontend/app/src/main/java/com/example/comp4521_project_gp4/ui/activity/MainActivity.kt
+++ android-frontend/app/src/main/java/com/example/comp4521_project_gp4/ui/activity/MainActivity.kt
@@ -13,34 +13,34 @@ import androidx.compose.ui.tooling.preview.Preview
 import com.example.comp4521_project_gp4.ui.theme.COMP4521ProjectGP4Theme
 
 class MainActivity : ComponentActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContent {
-            COMP4521ProjectGP4Theme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
-            }
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContent {
+      COMP4521ProjectGP4Theme {
+        // A surface container using the 'background' color from the theme
+        Surface(
+          modifier = Modifier.fillMaxSize(),
+          color = MaterialTheme.colorScheme.background
+        ) {
+          Greeting("Android")
         }
+      }
     }
+  }
 }
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+  Text(
+    text = "Hello $name!",
+    modifier = modifier
+  )
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
-    COMP4521ProjectGP4Theme {
-        Greeting("Android")
-    }
+  COMP4521ProjectGP4Theme {
+    Greeting("Android")
+  }
 }
