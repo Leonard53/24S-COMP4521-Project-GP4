e6aa2987e2eed2b06e2f22477002dc64c1bb250a
Jacky Cheung <jacky35207@gmail.com>
2024-05-10 23:03:04 +0800

    Added friends module

---
+++ android-frontend/app/src/main/java/com/comp4521_project_gp4/ui/activity/FriendsActivity.kt
@@ -0,0 +1,16 @@
+package com.comp4521_project_gp4.ui.activity
+import android.os.Bundle
+import android.widget.TextView
+import androidx.activity.ComponentActivity
+import android.content.Intent
+import com.comp4521_project_gp4.R
+import android.graphics.Color
+import android.view.View
+import com.google.android.material.card.MaterialCardView
+class FriendsActivity : ComponentActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    // Make the status bar transparent
+    setContentView(R.layout.activity_friends);
+  }
+}
---
+++ android-frontend/app/src/main/java/com/comp4521_project_gp4/viewmodel/FriendsViewModel.kt
@@ -0,0 +1,26 @@
+package com.comp4521_project_gp4.viewmodel
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.comp4521_project_gp4.model.FriendsModel
+
+class FriendViewModel : ViewModel() {
+//  private val _friends = MutableLiveData<List<FriendsModel.FriendsItem>>()
+//  val friends: LiveData<List<FriendsModel.FriendsItem>> = _friends
+//
+//  init {
+//    loadFriends()
+//  }
+//
+//  private fun loadFriends() {
+//    // Simulate loading data
+//    val friendList = listOf(
+//      FriendsModel("Alice"),
+//      FriendsModel("Bob"),
+//      FriendsModel("Charlie")
+//      // Add more friends as needed
+//    )
+//    _friends.value = friendList
+//  }
+}
