e9190dea6ec1470291861fc31a953313f25f4255
jacky35207 <44597523+jacky35207@users.noreply.github.com>
2024-05-11 22:04:33 +0800

    Date & time picker

--- android-frontend/app/src/main/java/com/comp4521_project_gp4/ui/activity/AddExerciseActivity.kt
+++ android-frontend/app/src/main/java/com/comp4521_project_gp4/ui/activity/AddExerciseActivity.kt
@@ -2,14 +2,19 @@ package com.comp4521_project_gp4.ui.activity
 
 import ToolbarFragment
 import android.os.Bundle
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 import com.comp4521_project_gp4.R
+import com.google.android.material.datepicker.MaterialDatePicker
+import com.google.android.material.textfield.TextInputEditText
+import com.google.android.material.timepicker.MaterialTimePicker
+import com.google.android.material.timepicker.TimeFormat
 
 class AddExerciseActivity : AppCompatActivity() {
+  
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
-    // Make the status bar transparent
-    setContentView(R.layout.activity_add_exercise);
+    setContentView(R.layout.activity_add_exercise)
     
     // Dynamically add the Toolbar Fragment
     if (savedInstanceState == null) {
@@ -17,5 +22,48 @@ class AddExerciseActivity : AppCompatActivity() {
         .replace(R.id.toolbar_container, ToolbarFragment())
         .commit()
     }
+    
+    val dateInput = findViewById<TextInputEditText>(R.id.exerciseDateEditText)
+    val durationInput = findViewById<TextInputEditText>(R.id.exerciseDurationEditText)
+    
+    // Set up click listeners for dateTimeEditText
+    dateInput.setOnClickListener {
+      showDatePicker()
+    }
+    
+    durationInput.setOnClickListener {
+      showTimePicker()
+    }
+    
+  }
+  
+  private fun showDatePicker() {
+    val dateInput = findViewById<TextInputEditText>(R.id.exerciseDateEditText)
+    
+    val datePicker = MaterialDatePicker.Builder.datePicker()
+      .setTitleText("Select date")
+      .build()
+    datePicker.addOnPositiveButtonClickListener { date ->
+      // Format the date and set it to the EditText
+      dateInput.setText(datePicker.headerText)
+    }
+    datePicker.show(supportFragmentManager, datePicker.toString())
+  }
+  
+  private fun showTimePicker() {
+    val durationInput = findViewById<TextInputEditText>(R.id.exerciseDurationEditText)
+    
+    val timePicker = MaterialTimePicker.Builder()
+      .setTitleText("Select Time")
+      .setTimeFormat(TimeFormat.CLOCK_24H)
+      .build()
+    timePicker.addOnPositiveButtonClickListener {
+      // You could format the time and append it to the date
+      val pickedHour = timePicker.hour
+      val pickedMinute = timePicker.minute
+      val timeString = String.format("%02d:%02d", pickedHour, pickedMinute)
+      durationInput.setText("$timeString")
+    }
+    timePicker.show(supportFragmentManager, timePicker.toString())
   }
 }
