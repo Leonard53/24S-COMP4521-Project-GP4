0432e4838d734bd5981ab99d60ee72f75f794fc8
Leoanrd Yeung <94115482+Leonard53@users.noreply.github.com>
2024-05-11 19:31:09 +0800

    feat: now cache signed in user

--- android-frontend/app/src/main/java/com/comp4521_project_gp4/ui/activity/MainActivity.kt
+++ android-frontend/app/src/main/java/com/comp4521_project_gp4/ui/activity/MainActivity.kt
@@ -22,7 +22,6 @@ class MainActivity : AppCompatActivity() {
     window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or
         View.SYSTEM_UI_FLAG_LAYOUT_STABLE
     setContentView(R.layout.activity_main)
-    
     val loggedInUser = intent.getParcelableExtra<User>("user")
     val calories_burned = findViewById<TextView>(R.id.info1_value)
     val calories_intake = findViewById<TextView>(R.id.info2_value)
@@ -62,6 +61,17 @@ class MainActivity : AppCompatActivity() {
     
     friends_btn.setOnClickListener {
       startActivity(Intent(this, FriendsActivity::class.java))
+    }
+    nutrition_btn.setOnClickListener {
+      // Create an Intent to start the AddFood Activity
+      val intent = Intent(this, AddFood::class.java)
+      startActivity(intent)
+    }
+    
+    leadboard_btn.setOnClickListener {
+      // Create an Intent to start the AddFood Activity
+      val intent = Intent(this, dashboard::class.java)
+      startActivity(intent)
     }
   }
   
